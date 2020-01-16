03ce428b7aba7ba2509445b4f030c2ffd9fbc6ff
Zafar Mahmood <zfar.mah@gmail.com>
2020-01-16 10:56:33 +0100

    initial commit for fuzzy-sklearn

--- testing_mapping_function.py
+++ testing_mapping_function.py
@@ -9,9 +9,11 @@ def train_test_split (data) :
     return train_test_split(x_data, y_data,
                      test_size=0.33, random_state=42)
 
-def dict_update () :
-    pass
-    pass
+def dict_population (accuracy ,
+                     confusion_matrix ,
+                     classification_report ) :
+    return {'accuracy':accuracy, 'confusion_matrix':confusion_matrix , 'classification_report':classification_report }
+    #return_dict.udpate({})
 
 
 def logistic_regression (data) :
@@ -21,35 +23,42 @@ def logistic_regression (data) :
 
     clf = LogisticRegression(random_state=0).fit(X_train ,y_train)
     y_pred = clf.predict(X_test)
-    return_dict = {}
+
     target_names = ['0','1']
 
-    return_dict.update({ 'lg accuracy':clf.score(X_test,y_test)})
+    return  dict_population(accuracy= clf.score(X_test,y_test) ,
+                    confusion_matrix= confusion_matrix(y_test, y_pred) ,
+                    classification_report= classification_report(y_test, y_pred, target_names=target_names)
+        )
+    #return_dict.update({ 'lg accuracy':clf.score(X_test,y_test)})
 
-    return_dict.update({'lg confusion_matrix':confusion_matrix(y_test, y_pred)})
-    return_dict.update({'lg classification_report': classification_report(y_test, y_pred, target_names=target_names)})
+    #return_dict.update({'lg confusion_matrix':confusion_matrix(y_test, y_pred)})
+    #return_dict.update({'lg classification_report': classification_report(y_test, y_pred, target_names=target_names)})
 
-    return return_dict
 
 def decision_tree_classifier(data):
     X_train, X_test, y_train, y_test = train_test_split(data)
 
     from sklearn.tree import DecisionTreeClassifier
     clf = DecisionTreeClassifier(random_state=0)
-    return_dict = {}
+   #return_dict = {}
     y_pred = clf.predict(X_test)
     target_names = {'0','1'}
-    return_dict.update({'rf accuracy': metrics.accuracy_score(y_test, y_pred)})
-    return_dict.update({'rf confusion_matrix': confusion_matrix(y_test, y_pred)})
-    return_dict.update({'rf classification_report': classification_report(y_test, y_pred, target_names=target_names)})
+    #return_dict.update({'rf accuracy': metrics.accuracy_score(y_test, y_pred)})
+    #return_dict.update({'rf confusion_matrix': confusion_matrix(y_test, y_pred)})
+    #return_dict.update({'rf classification_report': classification_report(y_test, y_pred, target_names=target_names)})
+    return dict_population(accuracy=metrics.accuracy_score(y_test, y_pred),
+                           confusion_matrix=confusion_matrix(y_test, y_pred),
+                           classification_report=classification_report(y_test, y_pred, target_names=target_names)
+                           )
 
-    return return_dict
+    #return return_dict
 
 def random_forest_classifier(data):
 
     from sklearn.ensemble import RandomForestClassifier
 
-    return_dict = {}
+    #return_dict = {}
     X_train, X_test, y_train, y_test = train_test_split(data)
 
     clf = RandomForestClassifier()
@@ -57,12 +66,15 @@ def random_forest_classifier(data):
     y_pred = clf.predict(X_test)
 
     target_names=['0','1']
+    return dict_population(accuracy=metrics.accuracy_score(y_test, y_pred),
+                           confusion_matrix=confusion_matrix(y_test, y_pred),
+                           classification_report=classification_report(y_test, y_pred, target_names=target_names)
+                           )
+    #return_dict.update({'rf accuracy': metrics.accuracy_score(y_test, y_pred) })
+    #return_dict.update({'rf confusion_matrix':confusion_matrix(y_test, y_pred)})
+    #return_dict.update({'rf classification_report' : classification_report(y_test, y_pred, target_names=target_names)})
 
-    return_dict.update({'rf accuracy': metrics.accuracy_score(y_test, y_pred) })
-    return_dict.update({'rf confusion_matrix':confusion_matrix(y_test, y_pred)})
-    return_dict.update({'rf classification_report' : classification_report(y_test, y_pred, target_names=target_names)})
-
-    return return_dict
+    #return return_dict
 
 def xgboost_classifier (data):
     from sklearn.ensemble import RandomForestClassifier
@@ -75,16 +87,20 @@ def naive_bayes_classifier(data):
     from sklearn.naive_bayes import GaussianNB
     X_train, X_test, y_train, y_test = train_test_split(data)
 
-    return_dict = {}
+    #return_dict = {}
     target_names = ['0', '1']
     gnb = GaussianNB()
     y_pred_gnb = gnb.fit(X_train, y_train).predict(X_test)
 
-    return_dict.update({'gnb accuracy': metrics.accuracy_score(y_test, y_pred_gnb)})
-    return_dict.update({'gnb confusion_matrix': confusion_matrix(y_test, y_pred_gnb)})
-    return_dict.update({'gnb classification_report': classification_report(y_test, y_pred_gnb, target_names=target_names)})
+    return dict_population(accuracy=metrics.accuracy_score(y_test, y_pred_gnb),
+                           confusion_matrix=confusion_matrix(y_test, y_pred_gnb),
+                           classification_report=classification_report(y_test, y_pred_gnb, target_names=target_names)
+                           )
+    #return_dict.update({'gnb accuracy': metrics.accuracy_score(y_test, y_pred_gnb)})
+    #return_dict.update({'gnb confusion_matrix': confusion_matrix(y_test, y_pred_gnb)})
+    #return_dict.update({'gnb classification_report': classification_report(y_test, y_pred_gnb, target_names=target_names)})
 
-    return return_dict
+    #return return_dict
 
 
 if __name__ == '__main__':
@@ -97,7 +113,7 @@ if __name__ == '__main__':
 
 
     for i in range (1,10,2):
-        value = list(map(lambda x : x(i), fun_list))
+        value = list(map(lambda x : x(i), classifier_functions))
         print(value)
 
 
